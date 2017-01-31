00288089262e5484ca184f49b5c23c06b6f5a6a7
jamesjbot <jongs.j@gmail.com>
2017-01-31 17:32:25 -0500

    Initial Commit

--- UIMotionEffect/ViewController.swift
+++ UIMotionEffect/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    let leftRightMin: CGFloat = -600.0
+    let leftRightMax: CGFloat = 600.0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        // Motion Effect
+        let leftRight: UIInterpolatingMotionEffect = UIInterpolatingMotionEffect(
+            keyPath: "center.x",
+            type: .tiltAlongHorizontalAxis)
+        leftRight.minimumRelativeValue = leftRightMin
+        leftRight.maximumRelativeValue = leftRightMax
+
+        // Create motion effect group
+        let meGroup: UIMotionEffectGroup = UIMotionEffectGroup()
+        meGroup.motionEffects = [leftRight]
+
+        // Add group to ImageView
+        imageView.addMotionEffect(meGroup)
     }
 
     override func didReceiveMemoryWarning() {
